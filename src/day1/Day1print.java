07624708f474b820dd83acf028e5b651c31be1ea
rlatmdals0513 <2011seungmin.k@gmail.com>
2025-01-04 11:16:50 +0900

    i dont know

---
+++ src/day1/Day1print.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Day1print {
+    public static void main(String[] args){
+        System.out.println("이름:김승민");
+        System.out.print("성별:남자 \n");
+        System.out.print("싫어하는 과목:사회 \n");
+        System.out.print("좋아하는 과목:국어 \n");
+    }
+}
