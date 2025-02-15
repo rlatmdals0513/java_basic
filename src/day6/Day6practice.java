3f7d744c3d10e308230640c49cdf263ad25e8d4a
rlatmdals0513 <2011seungmin.k@gmail.com>
2025-02-15 11:25:18 +0900

    Day6

--- src/day6/Day6practice.java
+++ src/day6/Day6practice.java
@@ -1,15 +1,53 @@
 package day6;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Day6practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i =1; i<=10;i++){
-            if(i%2==0){
-                System.out.println("i = " + i);
-                continue;
-            }
-        }
+//        for(int i =1; i<=10;i++){
+//            if(i%2==0){
+//                System.out.println("i = " + i);
+//                continue;
+//            }
+//        }
+//        System.out.print("숫자를 입력하시오");
+//        int i = sc.nextInt();
+//        int sum = 0;
+//        for (int j = 1; j <= i; j++) {
+//
+//            sum += j;
+//            if (sum > 50) {
+//                System.out.println("50초과임");
+//                break;
+//
+//            }
+//            System.out.println("지금합계" + sum);
+//
+//        }
+//        System.out.println("최종합계"+sum);
+        //3
+//        System.out.print("높이를 입력하세요");
+//        int i =sc.nextInt();
+//        for(int j =1;j<=i;j++){
+//            for(int m=1;m<=j;m++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        System.out.print("높이를 입력하세요");
+////        int h =sc.nextInt();
+////
+////        for(int i = 1;i <= h;i++){
+////            for(int j =h - i; j>= 1;j--){
+////                System.out.print(' ');
+////            }
+////            for(int j = 1; j <= (i * 2 - 1); j++) {
+////                System.out.print('*');
+////            }
+////            System.out.println();
+////        }
+
     }
 }
